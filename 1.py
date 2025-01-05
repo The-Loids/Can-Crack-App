499db25b022546d80c619f756ca0df7f72514efd
Nicole <92153483+fembrained@users.noreply.github.com>
2025-01-05 14:28:52 -0800

    Initial Upload
    V1.0

---
+++ 1.py
@@ -0,0 +1,10 @@
+import time    
+et = int(time.time())
+
+pepsi = ("""One (1) 
+
+Pepsi\n""")
+
+print(pepsi)
+print(f"<t:{et}:F>")
+input("Press Enter to exit...")
---
+++ can crack.bat
@@ -0,0 +1,7 @@
+@echo off
+title Can Crack Bot 9000
+
+echo 1: Pepsi
+echo 2: Monster Java Mocha
+set /p "id=Enter Number: "
+python %id%.py
